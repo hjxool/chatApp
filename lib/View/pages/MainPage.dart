936ada39f2ae0e51406ef811fdc775f3d41918a7
houjie <691638334@qq.com>
2025-09-25 17:52:53 +0800

    9.25

--- lib/View/pages/MainPage.dart
+++ lib/View/pages/MainPage.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart'; // 第三方图标非内置
 import '../../Utils/rpx.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  // 对应页面
+  final List<Widget> _pages = [
+    Placeholder(color: Colors.red),
+    Placeholder(color: Colors.white),
+    Placeholder(color: Colors.green),
+  ];
+  // 导航栏
+  final List<BottomNavigationBarItem> _bottomNavItems = [
+    BottomNavigationBarItem(icon: Icon(Symbols.chat), label: '聊天'),
+    BottomNavigationBarItem(icon: Icon(Symbols.money_bag), label: '记账'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +29,6 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ChatWithMe'),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.black),
-            onPressed: null,
-          ),
           IconButton(
             onPressed: null,
             icon: const Icon(Icons.add, color: Colors.black),
@@ -27,8 +42,19 @@ class MainPage extends StatelessWidget {
             color: Color(0xFFE5E5E5), // 微信风格发丝线,
           ),
         ),
+        backgroundColor: Colors.grey[200],
       ),
-      body: Placeholder(),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        items: _bottomNavItems,
+        backgroundColor: Colors.grey[200],
+      ),
     );
   }
 }
